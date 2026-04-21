fa377ea4be9c9f5d19b3663a4d3cd3843c6eedfa
Brianna Matey <briianna@bu.edu>
2026-04-20 21:58:14 -0400

    updated the authoriation steps

---
+++ app/loginPage/page.tsx
@@ -0,0 +1,13 @@
+
+
+export default function loginPage() {
+    <div>
+    <h3>Login</h3>
+    <p>UserName</p>
+        <input>Email</input>
+        <p>password</p>
+        <input>Password</input>
+        <p>Sign in with Google</p>
+
+    </div>
+}
