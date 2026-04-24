b8ffed6afeb6586bfa27090ace4615c9d504f457
Napasorn Visuthiwat <tkvisuth@bu.edu>
2026-04-24 11:51:02 -0400

    deleted auth

--- app/api/auth/[...nextauth]/route.ts
+++
@@ -1,7 +0,0 @@
-import { auth } from "@/auth";
-
-export default async function Home() {
-    const session = await auth();
-    const user = session?.user ?? null;
-
-}
--- auth.ts
+++
@@ -1,9 +0,0 @@
-import NextAuth from "next-auth";
-import Google from "@auth/core/providers/google"
-
-export const {handlers, auth, signIn, signOut} = NextAuth({
-    providers: [Google({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    })],
-})
